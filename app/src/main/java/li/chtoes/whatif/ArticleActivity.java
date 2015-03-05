1a2864c41eb8d8f353ba02f69f191fb3839be992
yegorf1 <yegorf1@gmail.com>
2015-03-05 14:03:55 +0300

    Open fragment method

--- app/src/main/java/li/chtoes/whatif/ArticleActivity.java
+++ app/src/main/java/li/chtoes/whatif/ArticleActivity.java
@@ -1,17 +1,16 @@
 package li.chtoes.whatif;
 
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.Toast;
 
 import java.io.IOException;
+import java.util.List;
 
 
 public class ArticleActivity extends ActionBarActivity {
@@ -21,17 +20,19 @@ public class ArticleActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_article);
 
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setIcon(R.drawable.ic_actionbar_icon);
+        }
+
         App.mainActivity = this;
 
-        Log.d(App.TAG, "Reading articles.");
         try {
-            for (ArticleInfo info : App.API.getArticleInfos()) {
-                Log.d(App.TAG, info.toString());
-            }
+            List<ArticleInfo> i = App.API.getArticleInfos();
+            openArticle(i.get(0));
         } catch (IOException e) {
             e.printStackTrace();
         }
-        Log.d(App.TAG, "Done reading articles.");
     }
 
 
@@ -44,5 +45,15 @@ public class ArticleActivity extends ActionBarActivity {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         return super.onOptionsItemSelected(item);
+    }
+
+    public void openArticle(ArticleInfo info) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+
+        transaction.replace(R.id.container, ArticleFragment.newInstance(info));
+        transaction.addToBackStack(null);
+
+        transaction.commitAllowingStateLoss();
     }
 }
