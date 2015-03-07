4e95cae1295c4dff782cdd29df7ca094a9a758e9
yegorf1 <yegorf1@gmail.com>
2015-03-07 21:06:02 +0300

    Added index to titles

--- app/src/main/java/li/chtoes/whatif/App.java
+++ app/src/main/java/li/chtoes/whatif/App.java
@@ -197,7 +197,7 @@ public class App {
 
             try {
                 for (ArticleInfo a : getArticleInfos()) {
-                    titles.add(a.getTitle());
+                    titles.add(a.getIndex() + ". " + a.getTitle());
                 }
             } catch (IOException e) {
                 e.printStackTrace();
